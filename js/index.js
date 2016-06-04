fe90ef2e3ca56a7e6a9986ec568765341e5d37b7
Oskar <oskarradonkimball@gmail.com>
2016-06-04 12:41:26 -0700

    Refactor randomNum

--- js/index.js
+++ js/index.js
@@ -2,14 +2,14 @@ const colors = ['#7AD1D1',
                 '#5AD95A',
                 '#FFEF4F']
 
-function randomNum(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+function randomNum() {
+    return Math.floor(Math.random() * (2 + 1));
 }
 
 $(function(){
   $('.post').hover(function() {
     var id = this.id;
-    $('body').css({ background: colors[randomNum(0,2)] });
+    $('body').css({ background: colors[randomNum()] });
     $('#' + id + '-bg').show();
 
   }, function() {
