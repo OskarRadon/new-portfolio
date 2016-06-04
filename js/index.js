af6cf978ce0230335a7ea1b68196a9db115e5f96
Oskar <oskarradonkimball@gmail.com>
2016-06-04 12:38:03 -0700

    Javascript to get random background color

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
+const colors = ['#7AD1D1',
+                '#5AD95A',
+                '#FFEF4F']
+
+function randomNum(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 $(function(){
   $('.post').hover(function() {
     var id = this.id;
