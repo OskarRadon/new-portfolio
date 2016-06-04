d973a70557e17160cbbdd8a3809abbcc6b4987bd
Oskar <oskarradonkimball@gmail.com>
2016-06-04 12:38:21 -0700

    JavaScript to toggle background color and bg div

--- js/index.js
+++ js/index.js
@@ -10,5 +10,12 @@ $(function(){
   $('.post').hover(function() {
     var id = this.id;
     console.log(id);
+    $('body').css({ background: colors[randomNum(0,2)] });
+    $('#' + id + '-bg').show();
+
+  }, function() {
+    var id = this.id;
+    $('body').css({ background: "white" });
+    $('#' + id + '-bg').hide();
   });
 });
