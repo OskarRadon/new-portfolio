132d6a99ca1f7c901611e8a064926622f721d4ce
Oskar <oskarradonkimball@gmail.com>
2016-06-04 11:49:33 -0700

    Console.log id of div on hover
    Just for testing

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
-// on document load
-
-// when you hover on a .post
-
-// get the id
-
-// show .bg div where class === id of hovered div 
+$(function(){
+  $('.post').hover(function() {
+    var id = this.id;
+    console.log(id);
+  });
+});
